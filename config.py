1f0c4f0aea7c8bfcef5f80b2b615ccfb7535a87d
snugwarms <none@none.com>
2024-11-26 20:22:47 -0800

    configs added, backing up before i fuck everything up

---
+++ config.py
@@ -0,0 +1,5 @@
+class config:
+    debug = False
+    tty = '/dev/tty.usbmodemNT20091014001' #tty address for the display
+    blinkspeed = 0.5 
+    scrollspeed = 0.2
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from config import config
 import serial
+import time
+import threading
+
 
 app = Flask(__name__)
-
-
+lock = threading.Lock()
+is_running = false
 
 def send_serial_data(str1, str2):
-    with serial.Serial('/dev/tty.usbmodemNT20091014001', 19200, timeout=1) as ser:
+    with serial.Serial(config.tty, 19200, timeout=1) as ser:
+        print(ser.name)         # check which port was really used
+        ser.write(f'\r{str1} \r'.encode())  # write str1
+        ser.write(str2.encode())            # write str2
+        ser.close()             # close port
+
+def send_greeting(str1, str2):
+    with serial.Serial(config.tty, 19200, timeout=1) as ser:
         print(ser.name)         # check which port was really used
         ser.write(f'\r{str1} \r'.encode())  # write str1
         ser.write(str2.encode())            # write str2
@@ -15,7 +26,7 @@ def send_serial_data(str1, str2):
 @app.route('/entering/<str2>', methods=['GET'])
 def entering(str2):
     str1 = "Now Entering:"  # You can set str1 to any fixed value or pass it as needed
-    send_serial_data(str1, str2)
+    send_greeting(str1, str2)
     return f"Sent str2: {str2} to the serial port", 200
 
 @app.route('/display/<str1>:<str2>', methods=['GET'])
